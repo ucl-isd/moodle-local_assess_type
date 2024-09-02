e44a4639a42678f9a797d7cdbb90d27782151084
Stuart Lamour <thisismyrice@gmail.com>
2024-09-02 11:10:15 +0100

    CTP-3719 backup and restore (#6)
    * CTP-3719 initial commit
    
    Currently restore not working
    
    * CTP-3719 backup/restore/duplicate
    
    * CTP-3719 PHPDocs
    
    * CTP-3719 PHPDocs 2
    
    ---------
    
    Co-authored-by: David Watson <14983002+watson8@users.noreply.github.com>

---
+++ backup/moodle2/backup_local_assess_type_plugin.class.php
@@ -0,0 +1,51 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version.
+ *
+ * @package    local_assess_type
+ * @copyright  2024 onwards University College London {@link https://www.ucl.ac.uk/}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author     Stuart Lamour <s.lamour@ucl.ac.uk>
+ */
+
+/**
+ * Database: assess_type(id, courseid, cmid, type)
+ * Backup assess_type
+ */
+class backup_local_assess_type_plugin extends backup_local_plugin {
+
+
+    /**
+     * Define structure.
+     * @return backup_plugin_element
+     * @throws base_element_struct_exception
+     */
+    protected function define_module_plugin_structure() {
+        $plugin = $this->get_plugin_element();
+        $pluginwrapper = new backup_nested_element($this->get_recommended_name(),
+            ['id'],
+            ['type']
+        );
+        $plugin->add_child($pluginwrapper);
+        $pluginwrapper->set_source_sql(
+            'SELECT id, type FROM {local_assess_type} WHERE cmid = :cmid AND type IS NOT NULL',
+            ['cmid' => backup::VAR_PARENTID]
+        );
+        return $plugin;
+    }
+}
---
+++ backup/moodle2/restore_local_assess_type_plugin.class.php
@@ -0,0 +1,61 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version.
+ *
+ * @package    local_assess_type
+ * @copyright  2024 onwards University College London {@link https://www.ucl.ac.uk/}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author     Stuart Lamour <s.lamour@ucl.ac.uk>
+ */
+
+/**
+ * Database: assess_type(id, courseid, cmid, type)
+ * Restore assess_type
+ */
+class restore_local_assess_type_plugin extends restore_local_plugin {
+
+    /**
+     * Define structure.
+     * @return array
+     */
+    protected function define_module_plugin_structure() {
+        $paths = [];
+        // The restore_path_element needs postfix of 'process_*', path in xml.
+        $pathname = 'plugin_local_assess_type_module';
+        $paths[] = new restore_path_element($pathname, "/module/$pathname");
+        return $paths;
+    }
+
+    /**
+     * Process restore data to DB.
+     * @param mixed $data
+     * @return void
+     * @throws dml_exception
+     */
+    public function process_plugin_local_assess_type_module($data) {
+        global $DB;
+        $data = (object)$data;
+        $table = 'local_assess_type';
+        // Record for update/insert.
+        $r = new \stdClass();
+        $r->type = $data->type;
+        $r->cmid = $this->task->get_moduleid();
+        $r->courseid = $this->task->get_courseid();
+        $DB->insert_record($table, $r);
+    }
+}
